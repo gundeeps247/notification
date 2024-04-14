68d934d1ad2e8a1518a2c7d6235f0dff724a6bcf
gundeeps247 <gundeepsinghm@gmail.com>
2024-04-14 17:50:46 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ webpush.setVapidDetails(
 );
 
 const corsOptions = {
-    origin: 'https://notification-zmvu.vercel.app', // Replace with your frontend origin
+    origin: 'https://notification-9ysb.vercel.app', // Replace with your frontend origin
   };
   
   app.use(cors(corsOptions));
